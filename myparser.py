053c337380d338e672ff08816411f7bb81026fc9
Krishna Kinnal <krishnakinnal@gmail.com>
2017-06-23 11:12:18 +0530

    Parser

--- myparser.py
+++ myparser.py
@@ -7,6 +7,9 @@ words=string.split()
 tokens=pos_tag(string.split())
 propernouns=[word for word,pos in tokens if pos=='NNP']
 n=len(propernouns)
+if n==0:
+    print ("\n\tNo proper nouns found :(\n\n")
+    exit(0)
 for i in range(n):
    itemno=1
    print ("\n\n\tThe search results for [[["+str(propernouns[i])+"]]] are...")
